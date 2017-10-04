e5e3e94012bdb67a040215f11367f4769407991d
Stuart J. Patching <sp917@imperial.ac.uk>
2017-10-04 16:51:15 +0100

    Vectorized code

--- advection.py
+++ advection.py
@@ -1,5 +1,4 @@
-import math
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 
 # we shall solve the advection equation using FTBS
@@ -8,36 +7,54 @@ import matplotlib.pyplot as plt
 
 # initial condition: y(x,0) = f(x) where we define f below
 
-def f(x):
-    if x<1 and x>=0:
-        return 1
-    else:
-        return 0
 X = 2 
+
+def f(x):
+    return 1 - (x%X>0.5) - (x%X<0.25)
+    
+
+
 N = 100
 M = 100
 δx = X/N 
-T = 1
+T = 10
 δt = T/M
-c = 1
+c = 0.9 # c = uδt/δx
+C = c*δx/δt
 
-x = numpy.zeros(N)
+x = np.zeros(N)
 for n in range(0,N):
     x[n] = n*δx
 
-y = numpy.zeros(shape=(M,N))
+y = np.zeros(shape=(M,N))
 
 #initialize:
 
-for n in range(0,N):
-    y[0][n] = f(x[n])
+y[0] = f(x)
+
+
+A = np.zeros(shape=(N,N))
+for n in range(1,M):
+    for k in range(0,M):
+        A[n][k] = (1-c)*(n==k) + c*(n==((k+1)%M))
+
+A[0][N-1] = 1
 
 #iterate
 for m in range(1,M):
-    y[m][0] = 1
-    for n in range(1,N):
-        y[m][n] = y[m-1][n] - c*(y[m-1][n] - y[m-1][n-1])
+    y[m] = np.dot(A,y[m-1]) 
 
-plt.plot(x,y[80])
+
+#now plot exact solution
+ytrue = np.copy(y)
+for m in range(0,M):
+    for n in range(0,N):
+        ytrue[m][n] = f(x[n]-C*m*δt)
+
+t = 9.99
+m = int(M*t/T)
+plt.axis([0,X,0,1.2])
+plt.plot(x,y[m])
+plt.plot(x,ytrue[m])
 plt.show()
 
